bcbe50292f3deb33e0fe676da9db5785a76a08bf
Nathee Sungthong-ngam <natee.live@gmail.com>
2021-06-13 23:08:11 +0700

    add promise to compute partnerAmount+feeAmount

--- apps-pretty-land/src/screens/customer/WorkScreen/payment.js
+++ apps-pretty-land/src/screens/customer/WorkScreen/payment.js
@@ -9,16 +9,21 @@ import {
   TextInput,
   ImageBackground,
   SafeAreaView,
+  KeyboardAvoidingView,
 } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import { Button, Text } from "react-native-elements"
 import Icon from "react-native-vector-icons/FontAwesome"
+import DropDownPicker from "react-native-dropdown-picker"
+import { TextInputMask } from "react-native-masked-text"
 
+import { getBankList } from "../../../data/apis"
 import { GetIcon } from "../../../components/GetIcons"
 import bgImage from "../../../../assets/bg.png"
 import firebase from "../../../../util/firebase"
 
 const PaymentForm = ({ navigation, route }) => {
+  const { navigate } = navigation
   const { item } = route.params
   const [partnerAmount, setPartnerAmount] = useState("")
   const [feeAmount, setFeeAmount] = useState("")
@@ -26,28 +31,41 @@ const PaymentForm = ({ navigation, route }) => {
 
   const [image, setImage] = useState(null)
   const [bank, setBank] = useState("")
+  const [bankList, setBankList] = useState(getBankList())
   const [transferAmount, setTransferAmount] = useState("")
   const [datetime, setDateTime] = useState("")
+  const [openSelectBank, setOpenSelectBank] = useState(false)
 
-  useEffect(() => {
-    const ref = firebase.database().ref(`posts/${item.id}/partnerSelect`)
-    ref.once("value", (snapshot) => {
+  const computeAmount = (snapshot) => {
+    return new Promise((resolve, reject) => {
       let totalAmount = 0
       snapshot.forEach((item, index) => {
         const amt = parseInt(item.val().amount)
         totalAmount = totalAmount + amt
       })
-      setPartnerAmount(totalAmount.toFixed(2))
+      resolve(totalAmount)
     })
-  }, [])
+  }
+
+  const getFeeAmountFromFirebase = () => {
+    return new Promise((resolve, reject) => {
+      const ref = firebase.database().ref("appconfig/fee_amount")
+      ref.once("value", (snapshot) => {
+        const feeAmt = parseInt(snapshot.val())
+        resolve(feeAmt)
+      })
+    })
+  }
 
   useEffect(() => {
-    const ref = firebase.database().ref("appconfig/fee_amount")
-    ref.once("value", (snapshot) => {
-      const feeAmount = parseFloat(snapshot.val()).toFixed(2)
-      setFeeAmount(feeAmount)
+    const ref = firebase.database().ref(`posts/${item.id}/partnerSelect`)
+    ref.on("value", async (snapshot) => {
+      const pAmount = await computeAmount(snapshot)
+      const fAmount = await getFeeAmountFromFirebase()
+      const netTotalAmt = parseInt(pAmount) + parseInt(fAmount)
 
-      const netTotalAmt = parseInt(partnerAmount)+parseInt(feeAmount)
+      setPartnerAmount(pAmount.toFixed(2))
+      setFeeAmount(fAmount.toFixed(2))
       setNetTotalAmount(netTotalAmt.toFixed(2))
     })
   }, [])
@@ -70,7 +88,6 @@ const PaymentForm = ({ navigation, route }) => {
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     })
@@ -80,6 +97,23 @@ const PaymentForm = ({ navigation, route }) => {
     }
   }
 
+  const saveCustomerPayment = () => {
+    if (!bank) {
+      Alert.alert("แจ้งเตือน", "กรุณาระบุธนาคารที่ท่านโอนเงิน")
+      return
+    }
+    if (!transferAmount) {
+      Alert.alert("แจ้งเตือน", "กรุณาระบุยอดเงินที่ท่านโอน")
+      return
+    }
+    if (!datetime) {
+      Alert.alert("แจ้งเตือน", "กรุณาระบุวันที่ และเวลาที่โอนเงิน")
+      return
+    }
+
+    navigate("Post-List")
+  }
+
   return (
     <ImageBackground
       source={bgImage}
@@ -87,128 +121,144 @@ const PaymentForm = ({ navigation, route }) => {
       resizeMode="stretch"
     >
       <SafeAreaView style={{ height: "100%" }}>
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <Text style={styles.topic}>โอนเงิน เพื่อชำระค่าบริการ</Text>
-        </View>
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          style={{ marginTop: 20 }}
+        <KeyboardAvoidingView
+          behavior="position"
+          keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 0}
         >
-          <View style={{ width: "90%", alignSelf: "center" }}>
-            <Text style={{ fontSize: 16, padding: 5 }}>
-              ยอดชำระสำหรับ Partner (0.00)
-            </Text>
-            <View style={styles.formControl}>
-              <GetIcon type="fa" name="dollar" />
-              <TextInput
-                style={styles.textInput}
-                placeholder="ยอดชำระสำหรับ Partner (0.00)"
-                value={partnerAmount}
-                onChangeText={(value) => setPartnerAmount(value)}
-                editable={false}
-              />
-            </View>
-            <Text style={{ fontSize: 16, padding: 5 }}>
-              ค่าธรรมเนียม (0.00)
-            </Text>
-            <View style={styles.formControl}>
-              <GetIcon type="fa" name="dollar" />
-              <TextInput
-                style={styles.textInput}
-                placeholder="ค่าธรรมเนียม (0.00)"
-                value={feeAmount}
-                onChangeText={(value) => setFeeAmount(value)}
-                editable={false}
-              />
-            </View>
-            <Text style={{ fontSize: 16, padding: 5 }}>รวมยอดชำระทั้งหมด</Text>
-            <View style={styles.formControl}>
-              <GetIcon type="fa" name="dollar" />
-              <TextInput
-                style={styles.textInput}
-                placeholder="รวมยอดชำระทั้งหมด"
-                value={netTotalAmount}
-                onChangeText={(value) => setNetTotalAmount(value)}
-                editable={false}
-              />
-            </View>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <Text style={styles.topic}>โอนเงิน เพื่อชำระค่าบริการ</Text>
           </View>
-          <View style={{ margin: 5, alignSelf: "center", width: "90%" }}>
-            <Text style={styles.optionsNameDetail}>ยืนยันข้อมูลการโอนเงิน</Text>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{ marginTop: 20 }}
+          >
+            <View style={{ width: "90%", alignSelf: "center" }}>
+              <Text style={{ fontSize: 16, padding: 5 }}>
+                ยอดชำระสำหรับ Partner (บาท)
+              </Text>
+              <View style={styles.formControl}>
+                <GetIcon type="fa" name="dollar" />
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="ยอดชำระสำหรับ Partner (บาท)"
+                  value={partnerAmount}
+                  onChangeText={(value) => setPartnerAmount(value)}
+                  editable={false}
+                />
+              </View>
+              <Text style={{ fontSize: 16, padding: 5 }}>
+                ค่าธรรมเนียม (บาท)
+              </Text>
+              <View style={styles.formControl}>
+                <GetIcon type="fa" name="dollar" />
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="ค่าธรรมเนียม (บาท)"
+                  value={feeAmount}
+                  onChangeText={(value) => setFeeAmount(value)}
+                  editable={false}
+                />
+              </View>
+              <Text style={{ fontSize: 16, padding: 5 }}>
+                รวมยอดชำระทั้งหมด
+              </Text>
+              <View style={styles.formControl}>
+                <GetIcon type="fa" name="dollar" />
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="รวมยอดชำระทั้งหมด"
+                  value={netTotalAmount}
+                  onChangeText={(value) => setNetTotalAmount(value)}
+                  editable={false}
+                />
+              </View>
+            </View>
+            <View style={{ margin: 5, alignSelf: "center", width: "90%" }}>
+              <Text style={styles.optionsNameDetail}>
+                ยืนยันข้อมูลการโอนเงิน
+              </Text>
 
-            <Text style={{ fontSize: 16, padding: 5 }}>ธนาคารที่โอนเงิน</Text>
-            <View style={styles.formControl}>
-              <GetIcon type="fa" name="bank" />
-              <TextInput
-                style={styles.textInput}
-                placeholder="ธนาคารที่โอนเงิน"
+              <Text style={{ fontSize: 16, padding: 5 }}>ธนาคารที่โอนเงิน</Text>
+              <DropDownPicker
+                placeholder="-- เลือกธนาคาร --"
+                open={openSelectBank}
+                setOpen={setOpenSelectBank}
                 value={bank}
-                onChangeText={(value) => setBank(value)}
+                setValue={setBank}
+                items={bankList}
+                setItems={setBankList}
+                textStyle={{ fontSize: 18 }}
+                searchable={false}
+                selectedItemContainerStyle={{ backgroundColor: "#facaff" }}
+                listMode="SCROLLVIEW"
               />
-            </View>
-            <Text style={{ fontSize: 16, padding: 5 }}>ยอดเงินโอน 0.00</Text>
-            <View style={styles.formControl}>
-              <GetIcon type="fa" name="dollar" />
-              <TextInput
-                style={styles.textInput}
-                placeholder="ยอดเงินโอน 0.00"
-                value={transferAmount}
-                onChangeText={(value) => setTransferAmount(value)}
-              />
-            </View>
-            <Text style={{ fontSize: 16, padding: 5 }}>
-              เวลาที่โอนเงิน (dd/MM/yyyy)
-            </Text>
-            <View style={styles.formControl}>
-              <GetIcon type="mi" name="date-range" />
-              <TextInput
-                style={styles.textInput}
-                placeholder="เวลาที่โอนเงิน (dd/MM/yyyy)"
-                value={datetime}
-                onChangeText={(value) => setDateTime(value)}
-              />
-            </View>
-            <Button
-              icon={
-                <Icon
-                  name="file"
-                  size={15}
-                  color="white"
-                  style={{ marginRight: 5 }}
-                />
-              }
-              buttonStyle={{ marginTop: 10 }}
-              title="เลือกไฟล์...สลิปสำหรับการโอนเงิน"
-              onPress={pickImage}
-            />
-          </View>
-          {image && (
-            <View style={{ alignContent: "center" }}>
-              <View style={{ alignSelf: "center", marginTop: 10 }}>
-                <Image
-                  source={{ uri: image }}
-                  style={{ width: 200, height: 250 }}
+              <Text style={{ fontSize: 16, padding: 5 }}>ยอดเงินโอน (บาท)</Text>
+              <View style={styles.formControl}>
+                <GetIcon type="fa" name="dollar" />
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="ยอดเงินโอน (บาท)"
+                  value={transferAmount}
+                  onChangeText={(value) => setTransferAmount(value)}
+                  keyboardType="number-pad"
                 />
               </View>
-              <View
-                style={{ alignSelf: "center", padding: 5, marginBottom: 10 }}
-              >
-                <Button
-                  icon={
-                    <Icon
-                      name="save"
-                      size={20}
-                      color="white"
-                      style={{ marginRight: 5 }}
-                    />
-                  }
-                  buttonStyle={styles.buttonConfirm}
-                  title="ยืนยันข้อมูลการโอนเงิน"
+              <Text style={{ fontSize: 16, padding: 5 }}>
+                เวลาที่โอนเงิน (dd/MM/yyyy HH:mm:ss)
+              </Text>
+              <View style={styles.formControl}>
+                <GetIcon type="mi" name="date-range" />
+                <TextInputMask
+                  type={"datetime"}
+                  options={{
+                    format: "DD/MM/YYYY HH:mm:ss",
+                  }}
+                  value={datetime}
+                  onChangeText={(text) => setDateTime(text)}
+                  style={styles.textInputStype}
                 />
               </View>
+              <Button
+                icon={
+                  <Icon
+                    name="file"
+                    size={15}
+                    color="white"
+                    style={{ marginRight: 5 }}
+                  />
+                }
+                buttonStyle={{ marginTop: 10 }}
+                title="เลือกไฟล์...สลิปสำหรับการโอนเงิน"
+                onPress={pickImage}
+              />
             </View>
-          )}
-        </ScrollView>
+            {image && (
+              <View style={{ alignContent: "center", height: 450 }}>
+                <View style={{ alignSelf: "center", marginTop: 10 }}>
+                  <Image
+                    source={{ uri: image }}
+                    style={{ width: 200, height: 250 }}
+                  />
+                </View>
+                <View style={{ alignSelf: "center", padding: 5 }}>
+                  <Button
+                    icon={
+                      <Icon
+                        name="save"
+                        size={20}
+                        color="white"
+                        style={{ marginRight: 5 }}
+                      />
+                    }
+                    buttonStyle={styles.buttonConfirm}
+                    title="ยืนยันข้อมูลการโอนเงิน"
+                    onPress={() => saveCustomerPayment()}
+                  />
+                </View>
+              </View>
+            )}
+          </ScrollView>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </ImageBackground>
   )
@@ -218,6 +268,7 @@ const styles = StyleSheet.create({
   buttonConfirm: {
     backgroundColor: "green",
     marginTop: 10,
+    width: "100%",
   },
   optionsNameDetail: {
     fontSize: 18,
@@ -241,9 +292,9 @@ const styles = StyleSheet.create({
   textInput: {
     backgroundColor: "white",
     width: 250,
-    textAlign: "center",
     fontSize: 16,
     marginVertical: 5,
+    marginLeft: 10,
   },
   topic: {
     marginTop: 20,
@@ -253,6 +304,12 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
+  },
+  textInputStype: {
+    height: 40,
+    width: "100%",
+    fontSize: 18,
+    marginLeft: 10,
   },
 })
 
