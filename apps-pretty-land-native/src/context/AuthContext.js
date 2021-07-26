8cf2dca6a63815ff623fbbd8388003c956d4da29
natee.live@gmail.com <natee.live@gmail.com>
2021-07-26 08:14:11 +0700

    remove native app

--- apps-pretty-land-native/src/context/AuthContext.js
+++
@@ -1,8 +0,0 @@
-import createDataContext from "./createDataContext"
-
-const authReducer = (prevState, action) => {}
-
-export const { Provider, Context } = createDataContext(authReducer, {
-  token: null,
-  username: "",
-})
