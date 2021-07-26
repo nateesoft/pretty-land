f9ac7bd59585c66911af191e0a4b400825f15612
natee.live@gmail.com <natee.live@gmail.com>
2021-07-26 08:15:06 +0700

    update env to default location

--- apps-pretty-land/src/Constants.js
+++ apps-pretty-land/src/Constants.js
@@ -2,7 +2,7 @@ export const AppConfig = {
   bgImage: require("../assets/bg.png"),
   iconImage: require("../assets/icon.png"),
   logoImage: require("../assets/logo.png"),
-  env: "demo",
+  env: "",
   MemberStatus: {
     newRegister: "new_register",
     newRegisterMessage: "รอออนุมัติ",
